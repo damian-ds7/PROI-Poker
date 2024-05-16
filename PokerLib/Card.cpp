0495d5386f6328d733504907c743fce3808cfd76
Kamil Marszałek <01187250@pw.edu.pl>
2024-05-16 11:08:25 +0200

    Implemented methods of Card class

--- PokerLib/Card.cpp
+++ PokerLib/Card.cpp
@@ -1,5 +1,59 @@
-//
-// Created by kamil on 16.05.2024.
-//
-
 #include "Card.h"
+
+Suit Card::suit() const noexcept {
+return m_suit;
+}
+
+Value Card::value() const noexcept {
+return m_value;
+}
+
+Card::Card(Suit suit, Value value) : m_suit(suit), m_value(value) {};
+
+bool Card::operator==(const Card& other) const noexcept {
+return m_value == other.m_value;
+}
+
+bool Card::operator!=(const Card& other) const noexcept {
+return !(*this == other);
+}
+
+bool Card::operator<(const Card& other) const noexcept {
+return m_value < other.m_value;
+}
+
+bool Card::operator>(const Card& other) const noexcept {
+return m_value > other.m_value;
+}
+
+bool Card::operator<=(const Card& other) const noexcept {
+return m_value <= other.m_value;
+}
+
+bool Card::operator>=(const Card& other) const noexcept {
+return m_value >= other.m_value;
+}
+
+bool Card::operator==(const Value& other) const noexcept {
+return m_value == other;
+}
+
+bool Card::operator!=(const Value& other) const noexcept {
+return m_value != other;
+}
+
+bool Card::operator<(const Value& other) const noexcept {
+return m_value < other;
+}
+
+bool Card::operator>(const Value& other) const noexcept {
+return m_value > other;
+}
+
+bool Card::operator<=(const Value& other) const noexcept {
+return m_value <= other;
+}
+
+bool Card::operator>=(const Value& other) const noexcept {
+return m_value >= other;
+}

533497ab85236e09877e5214c403f2256efd9a2f
Kamil Marszałek <01187250@pw.edu.pl>
2024-05-25 20:17:58 +0200

    Added function declarations for bot player

--- src/BotPlayer/BotPlayer.cpp
+++ src/BotPlayer/BotPlayer.cpp
@@ -1,1 +1,3 @@
 #include "BotPlayer.h"
+
+BotPlayer::BotPlayer(std::string name, unsigned int money, unsigned int bet) : Player(name, money, bet) {};
--- src/BotPlayer/BotPlayer.h
+++ src/BotPlayer/BotPlayer.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "Player.h"
+#include "simulator.h"
+
 
 class BotPlayer : public Player{
-
-
+    double m_probability;
+    Simulator m_simulator;
+public:
+    BotPlayer(std::string name, unsigned int money, unsigned int bet);
+    double get_probability();
+    void make_decision();
 };
 

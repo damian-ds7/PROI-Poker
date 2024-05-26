156237794c099d64fe3a3935201b38a553cd2e24
msuski <01187288@pw.edu.pl>
2024-05-26 14:02:51 +0200

    Card Graphics

---
+++ PokerUI/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_PokerUI_FILE C:/Users/susek/source/repos/24L-PROI-PROJECT-2-DSouza-Damian-Marszalek-Kamil-Suski-Michal/PokerUI/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/PokerUI.exe)
+set(__QT_DEPLOY_TARGET_PokerUI_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)

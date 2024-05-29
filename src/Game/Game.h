6c0dc6c7ac762cd710a895dc729b56485b124d9f
Damian D'Souza <damian.dsouza@pw.edu.pl>
2024-05-29 22:01:16 +0200

    Added Game class files

---
+++ src/Game/Game.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Player.h"
+#include "BotPlayer.h"
+#include "HumanPlayer.h"
+#include "Table.h"
+#include "CardCollection.h"
+#include "Card.h"
+#include "Deck.h"
+#include "CardsTypedef.h"
+
+class Game {};

1d32e4fda58a65ebc2dc2200528f85746f5170b8
msuski <01187288@pw.edu.pl>
2024-05-30 17:01:51 +0200

    Two pictures visible at once

--- PokerUI/main.cpp
+++ PokerUI/main.cpp
@@ -1,11 +1,29 @@
-#include "mainwindow.h"
+#include "widget.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    /*MainWindow w;
     w.show();
+
+    QLabel label(&w);
+    QPixmap background(":/resources/Deck/clubs_ace.png");
+    label.setPixmap(background);
+    label.setScaledContents(true);
+    label.raise();*/
+    Widget Main_Window;
+    QLabel label(&Main_Window);
+    QPixmap background(":/resources/Deck/clubs_ace.png");
+    label.setPixmap(background);
+    label.setScaledContents(true);
+    label.setBaseSize(background.size());
+
+    //QPushButton Bu_Quit("Quit", &Main_Window);
+
+    //QObject::connect(&Bu_Quit, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+    Main_Window.show();
     return a.exec();
 }

212ee655850e30628845e652462a5aa7e028e1bf
Damian D'Souza <damian.dsouza@pw.edu.pl>
2024-05-31 12:40:08 +0200

    Implemented basic Game methods

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,2 +1,66 @@
 #include "Game.h"
+#include <random>
+#include <string>
+
+//Game::Game(const std::string& name, unsigned int player_count, unsigned int starting_money) {
+//    this->player_count = player_count;
+//    players.push_back(std::make_unique<HumanPlayer>(name, starting_money, 0));
+//    for (unsigned int i = 1; i < player_count; ++i) {
+//        players.push_back(std::make_unique<BotPlayer>("Bot " + std::to_string(i), starting_money, 0));
+//    }
+//
+//    std::random_device rd;
+//    std::mt19937 gen(rd());
+//    std::uniform_int_distribution<unsigned int> dist(0, player_count - 1);
+//}
+
+void Game::add_table_card(unsigned int num_of_cards) {
+    for (unsigned int i = 0; i < num_of_cards; ++i) {
+        table->push_back(std::move(deck->front()));
+        deck->pop_front();
+    }
+}
+
+void Game::deal() {
+    for (int i = 0; i < 2; ++i) {
+        for (auto& player : players) {
+            player->add_card(std::move(deck->front()));
+            deck->pop_front();
+        }
+    }
+}
+
+void Game::next_phase() {
+    switch (phase) {
+        case Phase::PreFlop:
+            add_table_card(3);
+            phase = Phase::Flop;
+            break;
+        case Phase::Flop:
+            add_table_card(1);
+            phase = Phase::Turn;
+            break;
+        case Phase::Turn:
+            add_table_card(1);
+            phase = Phase::River;
+            break;
+        case Phase::River:
+            phase = Phase::Showdown;
+            break;
+        case Phase::Showdown:
+            break;
+    }
+}
+
+void Game::collect_bets() {
+    for (auto& player : players) {
+        pot += player->remove_bet();
+    }
+}
+
+void Game::next_player() {
+    do {
+        current_player = (current_player + 1) % player_count;
+    } while (players[current_player]->folded());
+}
 
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <vector>
+#include <memory>
 #include "Player.h"
 #include "BotPlayer.h"
 #include "HumanPlayer.h"
@@ -7,5 +9,24 @@
 #include "Card.h"
 #include "Deck.h"
 #include "CardsTypedef.h"
+#include "Phase.h"
 
-class Game {};
+class Game {
+private:
+    unsigned int player_count;
+    unsigned int current_player;
+    unsigned int pot = 0;
+    unsigned int dealer;
+    std::vector<std::unique_ptr<Player>> players;
+    Cards table = std::make_unique<Table<std::unique_ptr<Card>>>();
+    Cards deck = std::make_unique<Deck<std::unique_ptr<Card>>>(true);
+    Phase phase = Phase::PreFlop;
+public:
+//    Game(const std::string& name, unsigned int player_count, unsigned int starting_money);
+    Game() = default;
+    void add_table_card(unsigned int num_of_cards);
+    void deal();
+    void next_phase();
+    void collect_bets();
+    void next_player();
+};

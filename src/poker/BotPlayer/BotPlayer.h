093e90be1e7b3c0a2803599eb47ad142c5756984
Kamil Marszałek <01187250@pw.edu.pl>
2024-06-05 22:44:07 +0200

    Added make_bluff function

--- src/poker/BotPlayer/BotPlayer.h
+++ src/poker/BotPlayer/BotPlayer.h
@@ -2,11 +2,15 @@
 #include "Player.h"
 #include <string>
 #include <vector>
+#include <random>
 #include "EquityCalculator.h"
 #include "CardRange.h"
 
 
 class BotPlayer : public Player{
+    std::random_device rd;
+    std::mt19937 engine;
+    std::uniform_int_distribution<int> dist;
     double m_equity;
 public:
     BotPlayer(std::string name, unsigned int money, unsigned int bet);
@@ -15,7 +19,8 @@ public:
     [[nodiscard]] std::vector<omp::CardRange> get_bot_hand(int num_of_players) const noexcept;
     omp::EquityCalculator calc;
     void calc_equity(const std::string& board_cards, int num_of_players);
-    void make_decision(unsigned int money_to_bet, unsigned int num_of_players, std::string);
+    void make_decision(unsigned int money_to_bet, unsigned int num_of_players, const std::string&);
+    void make_bluff(unsigned int money_to_bet, unsigned int num_of_players, const std::string&);
     //[[nodiscard]] std::vector<string> get_table_hand() const noexcept;
 };
 

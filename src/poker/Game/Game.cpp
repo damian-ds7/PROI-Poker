2fb34df319a113dd7b3003730786eb36ce643777
Damian D'Souza <damian.dsouza@pw.edu.pl>
2024-06-05 20:19:47 +0200

    Imlpemented game logic methods

--- src/poker/Game/Game.cpp
+++ src/poker/Game/Game.cpp
@@ -5,6 +5,7 @@
 
 Game::Game(const std::string& name, unsigned int player_count, unsigned int initial_money) {
     this->player_count = player_count;
+    this->currently_playing = player_count;
     players.push_back(std::make_unique<HumanPlayer>(name, initial_money, 0));
     for (unsigned int i = 1; i < player_count; ++i) {
         players.push_back(std::make_unique<BotPlayer>("Bot " + std::to_string(i), initial_money, 0));
@@ -19,6 +20,14 @@ Game::Game(const std::string& name, unsigned int player_count, unsigned int init
     players[dealer]->set_dealer(true);
     players[(dealer + 1) % player_count]->set_small_blind(true);
     players[(dealer + 2) % player_count]->set_big_blind(true);
+}
+
+unsigned int Game::find_active_player(unsigned int index) {
+    unsigned int active_player = index;
+    do {
+        active_player = (active_player + 1) % player_count;
+    } while (players[active_player]->folded());
+    return active_player;
 }
 
 void Game::add_table_card(unsigned int num_of_cards) {
@@ -69,14 +78,35 @@ void Game::collect_bets() {
 }
 
 void Game::next_player() {
-    do {
-        current_player = (current_player + 1) % player_count;
-    } while (players[current_player]->folded());
-    players[current_player]->reset_status();
+    if (players[current_player]->folded()) {
+        --currently_playing;
+    }
+
+    if (check_round_end()) {
+        next_phase();
+        currently_playing = find_active_player(dealer);
+    }
+
+    current_player = find_active_player(current_player);
 }
 
 void Game::bot_play() {
     unsigned int previous_bet = players[(current_player - 1) % player_count]->bet();
-    dynamic_cast<BotPlayer&>(*players[current_player]).make_decision(previous_bet, player_count, table->to_string());
-    next_player();
+    dynamic_cast<BotPlayer&>(*players[current_player]).make_decision(previous_bet, currently_playing    , table->to_string());
+}
+
+bool Game::check_round_end() {
+    bool equal_bets = true;
+    auto current_bet = players[current_player]->bet();
+    for (const auto& player : players) {
+        if (!player->folded() && player->bet() < current_bet) {
+            equal_bets = false;
+            break;
+        }
+    }
+    return equal_bets;
+}
+
+void Game::find_winner() {
+
 }
--- src/poker/Game/Game.h
+++ src/poker/Game/Game.h
@@ -14,13 +14,17 @@
 class Game {
 private:
     unsigned int player_count;
+    unsigned int currently_playing;
     unsigned int current_player;
     unsigned int pot = 0;
     unsigned int dealer;
+    std::vector<unsigned int> winners;
     std::vector<std::unique_ptr<Player>> players;
     Cards table = std::make_unique<Table<std::unique_ptr<Card>>>();
     Cards deck = std::make_unique<Deck<std::unique_ptr<Card>>>(true);
     Phase phase = Phase::PreFlop;
+
+    unsigned int find_active_player(unsigned int index);
 public:
     Game(const std::string& name, unsigned int player_count, unsigned int initial_money);
     Game() = default;
@@ -30,6 +34,11 @@ public:
     void collect_bets();
     void next_player();
     void bot_play();
+    void find_winner();
+    void find_winning_sequence(unsigned int player, unsigned int code);
+
+    bool check_round_end();
+
 
     friend class MainWindow;
     friend class GameHandler;

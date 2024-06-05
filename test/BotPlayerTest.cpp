3924a34303413e6cb33ea51639c7d44e6ea870f6
Kamil Marszałek <01187250@pw.edu.pl>
2024-06-05 22:54:14 +0200

    Changed tests for BotPlayer

--- test/BotPlayerTest.cpp
+++ test/BotPlayerTest.cpp
@@ -129,7 +129,7 @@ TEST(BotPlayerTest, BotPlayerMakeDecisionFold){
     std::unique_ptr<Card> card2 = std::make_unique<Card>(Suit::Spades, Value::Three);
     bot.add_card(std::move(card1));
     bot.add_card(std::move(card2));
-    bot.make_decision(100, 2, "7h8h9h");
+    bot.make_decision(100, 2, "7h8h9h", false);
     EXPECT_EQ(bot.bet(), 0);
 }
 

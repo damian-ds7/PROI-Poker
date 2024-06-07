e230da1ec566094c2e8c22f328e9a9f95f7e4fc9
Kamil Marszałek <01187250@pw.edu.pl>
2024-06-07 13:02:20 +0200

    Tried to fix test for GameHandler::phase_to_int()

--- test/GameHandlerTest.cpp
+++ test/GameHandlerTest.cpp
@@ -178,15 +178,19 @@ TEST(GameHandlerTest, GameHandlerPhaseToInt){
     game_handler.start_game();
     game_handler.play_turn(Decision::Raise, 10);
     game_handler.play_turn(Decision::Call, 10);
+    game_handler.play_turn(Decision::Call, 0);
     EXPECT_EQ(game_handler.phase_to_int(), 1);
     game_handler.play_turn(Decision::Raise, 10);
     game_handler.play_turn(Decision::Call, 10);
+    game_handler.play_turn(Decision::Call, 0);
     EXPECT_EQ(game_handler.phase_to_int(), 2);
     game_handler.play_turn(Decision::Raise, 10);
     game_handler.play_turn(Decision::Call, 10);
+    game_handler.play_turn(Decision::Call, 0);
     EXPECT_EQ(game_handler.phase_to_int(), 3);
     game_handler.play_turn(Decision::Raise, 10);
     game_handler.play_turn(Decision::Call, 10);
+    game_handler.play_turn(Decision::Call, 0);
     EXPECT_EQ(game_handler.phase_to_int(), 4);
 }
 

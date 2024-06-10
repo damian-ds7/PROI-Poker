81ccea3015372b304a945b5f3de65f74e0a6456c
Damian D'Souza <damian.dsouza@pw.edu.pl>
2024-06-10 18:40:34 +0200

    Modified add_cards method

--- src/poker/CardCollection/CardCollection.h
+++ src/poker/CardCollection/CardCollection.h
@@ -19,8 +19,14 @@ public:
         std::deque<Ptr>::push_back(std::move(card));
     }
 
-    virtual void add_cards(std::deque<Ptr> cards) {
-        for (auto& card : cards) {
+//    virtual void add_cards(std::deque<Ptr> cards) {
+//        for (auto& card : cards) {
+//            std::deque<Ptr>::push_back(std::move(card));
+//        }
+//    }
+
+    virtual void add_cards(std::unique_ptr<CardCollection<Ptr>> cards) {
+        for (auto& card : *cards) {
             std::deque<Ptr>::push_back(std::move(card));
         }
     }
